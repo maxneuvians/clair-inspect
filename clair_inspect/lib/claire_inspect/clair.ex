ab7e25e404069a2c7662878a3c674e8592df3187
Max <max@neuvians.net>
2019-09-09 15:27:48 -0400

    Replaced protobufs

--- clair_inspect/lib/claire_inspect/clair.ex
+++ clair_inspect/lib/claire_inspect/clair.ex
@@ -1,3 +1,26 @@
-defmodule ClairInspect.Clair.Messages do
-  use Protobuf, from: [Path.expand("proto/timestamp.proto", __DIR__),Path.expand("proto/clair.proto", __DIR__)]
+defmodule ClairInspect.Clair do
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "http://localhost:6060"
+  plug Tesla.Middleware.JSON
+
+  def add_layer(url, parent \\ "") do
+    name =
+      url
+      |> String.split("?")
+      |> hd()
+      |> String.split("/")
+      |> Enum.reverse()
+      |> hd()
+
+    data = %{
+      "Layer": %{
+        "Name": name,
+        "Path": url,
+        "ParentName": parent,
+        "Format": "Docker"
+      }
+    }
+    post("/v1/layers", Jason.encode!(data))
+  end
 end
